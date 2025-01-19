9eb5f220f29c3406a11ed4e0bc5a11975c640c3f
Jordan-type <37406048+Jordan-type@users.noreply.github.com>
2025-01-19 23:22:57 +0300

    added next/navigation on the category page

--- src/app/r/donate/category/[category]/page.tsx
+++ src/app/r/donate/category/[category]/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from "next/navigation";
 
 const CategoryPage: React.FC = () => {
     const router = useRouter();
